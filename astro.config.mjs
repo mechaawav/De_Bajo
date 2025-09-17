088c5bd1439bef0b79a9ccf97857ae7212a7fc15
Rosario <rosario.egui@gmail.com>
2025-09-17 19:36:09 -0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   adapter: vercel(),
+  output: 'hybrid',   // para que funcionen /api en Vercel
   integrations: [react()],
 });
